78ba99a131e8430b7f925326a8c7d00e4556068d
Julian <juliand.montoyav@uqvirtual.edu.co>
2025-03-27 20:49:18 -0500

    se le agreraron atributos a la clase director y los implementamos en el main

--- src/main/java/co/edu/uniquindio/poo/Director.java
+++ src/main/java/co/edu/uniquindio/poo/Director.java
@@ -1,5 +1,55 @@
 package co.edu.uniquindio.poo;
 
 public class Director {
+    private String nombre;
+	private String id;
+	private int edad;
 
+    public Director(){
+    }
+	
+	
+	public Director(String nombre , String id , int edad) {
+		this.nombre = nombre;
+		this.id = id;
+		this.edad = edad;
+		
+		
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public void mensajePrueba(){
+		System.out.println("Hola");
+    }
 }
+
+
--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -3,5 +3,17 @@ package co.edu.uniquindio.poo;
 public class Main {
     public static void main(String[] args) {
     System.out.println("Bienvenidos al laboratorio de Git con Java");
+    Director mensaje = new Director();
+    mensaje.mensajePrueba();
     }
-    }
+
+}
+    
+        
+    
+    
+       
+    
+    
+    
+    
