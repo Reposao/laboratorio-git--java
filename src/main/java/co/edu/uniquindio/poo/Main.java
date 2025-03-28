e0d36d09ec39964f88b07fac056e8f6ea74db023
Isaq <isabela.quinterom@uqvirtual.edu.co>
2025-03-27 20:58:44 -0500

    creacion clase secretari

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.poo;
 
 public class Main {
+
     public static void main(String[] args) {
     System.out.println("Bienvenidos al laboratorio de Git con Java");
+
+    secretaria mensaje = new secretaria ();
+    mensaje.mostrarMensaje();
     }
-    }
+ }
